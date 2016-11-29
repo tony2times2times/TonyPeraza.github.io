1400ded6b117c9a7c3cf10915cc7c80587934a29
tony2times2times <tony.peraza86@gmail.com>
2016-11-29 13:38:02 -0600

    replaced intro with links tab

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,21 @@
-$(document).ready(function(){
-console.log("Document loaded");
+$(document).ready(function() {
+    console.log("Document loaded");
 
-    $("button").filter('.showIntro').click(function(){
-      console.log("working");
-      $('.info').hide("slow");
-      $('#intro').show("slow");
+    $("button").filter('#showIntro').click(function() {
+        console.log("working");
+        $('.info').hide("slow");
+        $('#links').show("slow");
     });
 
-    $("button").filter('.showEdu').click(function(){
-      console.log("working");
-      $('.info').hide("slow");
-      $('#edu').show("slow");
+    $("button").filter('#showEdu').click(function() {
+        console.log("working");
+        $('.info').hide("slow");
+        $('#edu').show("slow");
     });
-    $("button").filter('.showExp').click(function(){
-      console.log("working");
-      $('.info').hide("slow");
-      $('#exp').show("slow");
+    $("button").filter('#showExp').click(function() {
+        console.log("working");
+        $('.info').hide("slow");
+        $('#exp').show("slow");
     });
 
 
